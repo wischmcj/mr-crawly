7541e50bf6013912d041cf5373b1044e5e04af89
Collin Wischmcj <cjwischmeyer@gmail.com>
2025-05-08 15:31:03 -0400

    adding create dir function to utils

--- mr_crawly/utils.py
+++ mr_crawly/utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from urllib.parse import urlparse
 
 
@@ -7,3 +8,16 @@ def parse_url(url: str):
     """Parse a url and return the page elements"""
     parsed_url = urlparse(url)
     return parsed_url.scheme, parsed_url.netloc, parsed_url.path
+
+
+def create_dir(dir_name):
+    # Method 1: Using os.mkdir() to create a single directory
+    dir_name = "my_new_directory"
+    try:
+        os.mkdir(dir_name)
+        print(f"Directory '{dir_name}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{dir_name}' already exists.")
+    except PermissionError as err:
+        print(f"Permission denied: Unable to create '{dir_name}'.")
+        raise err
