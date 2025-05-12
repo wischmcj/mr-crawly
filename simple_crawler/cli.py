905f94f1a38d87e3cb5f166cd357ac1aa9df94be
Collin Wischmcj <cjwischmeyer@gmail.com>
2025-05-12 14:43:21 -0400

    adding asuncio call to cli. Adding checkevery and write to db options

--- simple_crawler/cli.py
+++ simple_crawler/cli.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import argparse
+import asyncio
 
 from config.logging_config import get_logger
-from main_pc import crawl
+
+from simple_crawler.main import crawl
 
 
 def main():
@@ -12,12 +14,28 @@ def main():
     parser = argparse.ArgumentParser(description="Basic Web Crawler")
     parser.add_argument("url", help="Starting URL to crawl")
     parser.add_argument(
-        "--max-pages", type=int, default=10, help="Maximum number of pages to crawl"
+        "--max-pages", type=int, default=100, help="Maximum number of pages to crawl"
     )
     parser.add_argument(
         "--retries", type=int, default=3, help="Delay between requests in seconds"
     )
+    parser.add_argument(
+        "--write_to_db",
+        type=bool,
+        default=True,
+        help="Determimes if results are saved to a sqlite db",
+    )
+    parser.add_argument(
+        "--check_every", type=float, default=0.5, help="Delay between checks in seconds"
+    )
 
     args = parser.parse_args()
 
-    crawl(args.url, args.max_pages, args.retries)
+    links = asyncio.run(
+        crawl(
+            args.url, args.max_pages, args.retries, args.write_to_db, args.check_every
+        )
+    )
+    for link in links:
+        print(link)
+    print(f"Crawled {len(links)} pages")
