434d2c39d3069203e3944eeec85e5c2b6354f4c5
Collin Wischmcj <cjwischmeyer@gmail.com>
2025-05-12 08:02:41 -0400

    adding base listener class for event based processing

---
+++ simple_crawler/helper_classes.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import threading
+from abc import abstractmethod
+
+from config.configuration import get_logger
+
+logger = get_logger("scheduler")
+
+
+class BaseListener:
+    """
+    Subscribes to a Redis stream and performs an action
+    on each message.
+    """
+
+    def __init__(self, pubsub, queue):
+        """Initialize with a Redis pubsub object"""
+        self.pubsub = pubsub
+        self.queue = queue
+        self.running = True
+
+    def start(self):
+        """Start the message handling loop in a separate thread"""
+        self.thread = threading.Thread(target=self.handle_messages)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def stop(self):
+        """Stop the message handling loop"""
+        self.running = False
+        self.thread.join()
+
+    @abstractmethod
+    def handle_message(self, message):
+        """Handle a message"""
+        pass
