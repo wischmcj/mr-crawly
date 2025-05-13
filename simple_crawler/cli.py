57c0f9bf32a6064e1970c25d3f7478684ad53899
Collin Wischmcj <cjwischmeyer@gmail.com>
2025-05-13 00:11:03 -0400

    finalizing cli

--- simple_crawler/cli.py
+++ simple_crawler/cli.py
@@ -2,37 +2,27 @@ from __future__ import annotations
 
 import argparse
 
-from config.logging_config import get_logger
+from config.configuration import CHECK_EVERY, MAX_PAGES, RETRIES, get_logger
+from main import crawl
 
-from simple_crawler.main import crawl
-
-
-def main():
-    logger = get_logger("main")
-    logger.info("Starting crawler")
-    parser = argparse.ArgumentParser(description="Basic Web Crawler")
-    parser.add_argument("url", help="Starting URL to crawl")
-    parser.add_argument(
-        "--max-pages", type=int, default=100, help="Maximum number of pages to crawl"
-    )
-    parser.add_argument(
-        "--retries", type=int, default=3, help="Delay between requests in seconds"
-    )
-    parser.add_argument(
-        "--write_to_db",
-        type=bool,
-        default=True,
-        help="Determimes if results are saved to a sqlite db",
-    )
-    parser.add_argument(
-        "--check_every", type=float, default=0.5, help="Delay between checks in seconds"
-    )
-
-    args = parser.parse_args()
-
-    links = crawl(
-        args.url, args.max_pages, args.retries, args.write_to_db, args.check_every
-    )
-    for link in links:
-        print(link)
-    print(f"Crawled {len(links)} pages")
+logger = get_logger("main")
+logger.info("Starting crawler")
+parser = argparse.ArgumentParser(description="Basic Web Crawler")
+parser.add_argument("url", help="Starting URL to crawl")
+parser.add_argument(
+    "--max-pages", type=int, default=MAX_PAGES, help="Maximum number of pages to crawl"
+)
+parser.add_argument(
+    "--retries", type=int, default=RETRIES, help="Delay between requests in seconds"
+)
+parser.add_argument(
+    "--check_every",
+    type=float,
+    default=CHECK_EVERY,
+    help="Delay between checks in seconds",
+)
+args = parser.parse_args()
+links = crawl(args.url, args.max_pages, args.retries, args.check_every)
+for link in links:
+    logger.info(link)
+logger.info(f"Crawled {len(links)} pages")
